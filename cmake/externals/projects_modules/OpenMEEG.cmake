8f6a95fb6b9ef039cc9666f0376d4f1f7aef8347
Emmanuel Olivi <Emmanuel.olivi@inria.fr>
2017-03-17 16:11:02 +0100

    MKL for windows

--- cmake/externals/projects_modules/OpenMEEG.cmake
+++ cmake/externals/projects_modules/OpenMEEG.cmake
@@ -11,7 +11,7 @@ function(OpenMEEG_project)
 
     # Prepare the project and list dependencies
 
-    EP_Initialisation(OpenMEEG BUILD_SHARED_LIBS ON)
+    EP_Initialisation(OpenMEEG BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
     EP_SetDependencies(${ep}_dependencies clapack matio ${MSINTTYPES})
 
     # No need to define repository where get the sources, since they are integrated.
@@ -34,6 +34,9 @@ function(OpenMEEG_project)
         -DUSE_VTK:BOOL=${USE_VTK}
         -DUSE_GIFTI:BOOL=${USE_GIFTI}
         -DUSE_CGAL:BOOL=${USE_CGAL}
+        -DMKL_USE_sdl:BOOL=${MKL_USE_sdl}
+        -DMKL_USE_interface:STRING=${MKL_USE_interface}
+        -DMKL_USE_parallel:BOOL=${MKL_USE_parallel}
         -DUSE_PROGRESSBAR:BOOL=${USE_PROGRESSBAR}
         -DENABLE_PACKAGING:BOOL=${ENABLE_PACKAGING}
         -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON}
