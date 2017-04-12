60ce1043a3d6971ca45ff7f7cfbc70eec698e90b
Emmanuel Olivi <Emmanuel.olivi@inria.fr>
2017-04-12 16:12:34 +0200

    finishing on packaging correctly on linux (RPM,DEB) 1/

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -5,35 +5,28 @@
 option(ENABLE_PACKAGING "Enable Packaging" ON)
 
 if (ENABLE_PACKAGING)
-    set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/packaging ${CMAKE_MODULE_PATH})
+    # set(CPACK_RPM_PACKAGE_DEBUG TRUE)
 
-    set(CPACK_PACKAGE_NAME "OpenMEEG")
-    set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "OpenMEEG Project")
-    set(CPACK_PACKAGE_DESCRIPTION "A C++ package for low-frequency bio-electromagnetism solving forward problems in the field of EEG and MEG.")
-    set(CPACK_PACKAGE_VENDOR "INRIA-ENPC")
-    set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
-    set(CPACK_PACKAGE_DESCRIPTION_FILE "${PROJECT_SOURCE_DIR}/README.rst")
-    set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE.txt")
-    set(CPACK_PACKAGE_CONTACT "openmeeg-info@lists.gforge.inria.fr")
-    set(CPACK_PACKAGE_INSTALL_DIRECTORY "OpenMEEG")
-    set(CPACK_SOURCE_STRIP_FILES "")
+    set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/OpenMEEG/cmake/packaging ${CMAKE_MODULE_PATH})
 
-    # set platform specific variables
-    if (UNIX)
-        #set(CPACK_SET_DESTDIR ON) <-- needed for TGZ
-        set(SYSTEMDIR linux)
-        if (APPLE)
-            set(SYSTEMDIR apple)
-        endif()
+    # load variables
+    include(PackagingOpenMEEG)
+
+    set(CPACK_GENERATOR "TGZ")
+
+    # if we want to generate all the sub-project packages:
+    #set(CPACK_INSTALL_CMAKE_PROJECTS "${CMAKE_CURRENT_BINARY_DIR}/matio/build;matio;ALL;/" "${CMAKE_CURRENT_BINARY_DIR}/OpenMEEG/build;OpenMEEG;ALL;/")
+
+    # call cpack for OpenMEEG
+    set(CPACK_INSTALL_CMAKE_PROJECTS "${CMAKE_CURRENT_BINARY_DIR}/OpenMEEG/build;OpenMEEG;ALL;/")
+
+    set(PACKAGE_OPTIONS ${BLASLAPACK_IMPLEMENTATION})
+
+    if (BUILD_SHARED_LIBS)
+        set(PACKAGE_OPTIONS ${PACKAGE_OPTIONS}-shared)
     else()
-        set(CPACK_SET_DESTDIR false)
-        set(CPACK_INSTALL_PREFIX "")
-        set(SYSTEMDIR windows)
+        set(PACKAGE_OPTIONS ${PACKAGE_OPTIONS}-static)
     endif()
-
-    include(${SYSTEMDIR}/PackagingConfiguration)
-
-    set(PACKAGE_OPTIONS ${PACKAGE_NAME}-${BLASLAPACK_IMPLEMENTATION})
 
     if (USE_OMP)
         set(PACKAGE_OPTIONS ${PACKAGE_OPTIONS}-OpenMP)
@@ -43,17 +36,15 @@ if (ENABLE_PACKAGING)
         set(PACKAGE_OPTIONS ${PACKAGE_OPTIONS}-vtk)
     endif()
 
+    if (USE_CGAL)
+        set(PACKAGE_OPTIONS ${PACKAGE_OPTIONS}-cgal)
+    endif()
+
     if (ENABLE_PYTHON)
         set(PACKAGE_OPTIONS ${PACKAGE_OPTIONS}-python)
     endif()
 
-    if (BUILD_SHARED_LIBS)
-        set(PACKAGE_OPTIONS ${PACKAGE_OPTIONS}-shared)
-    else()
-        set(PACKAGE_OPTIONS ${PACKAGE_OPTIONS}-static)
-    endif()
-
-    set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${PACKAGE_ARCH_SHORT}${PACKAGE_OPTIONS}")
+    set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${PACKAGE_ARCH_SHORT}-${PACKAGE_OPTIONS}")
 
     include(InstallRequiredSystemLibraries)
     include(CPack)
